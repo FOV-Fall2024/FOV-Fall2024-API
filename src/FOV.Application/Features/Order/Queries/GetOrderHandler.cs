8cc3698179c63d43487947dc0937390c7eb6c14b
ducanhnt22 <ducanhbon4@gmail.com>
2024-07-29 12:50:25 +0700

    feat: Order - not done yet

---
+++ src/FOV.Application/Features/Order/Queries/GetOrderHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOV.Application.Features.Order.Queries;
+public class GetOrderHandler
+{
+}
