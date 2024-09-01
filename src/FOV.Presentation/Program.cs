7cce82cd343052adf46e39c82520c53863cba482
Man Vo <99975060+Nang13@users.noreply.github.com>
2024-09-01 14:11:20 +0700

    build: addCheckLog

--- src/FOV.Presentation/Program.cs
+++ src/FOV.Presentation/Program.cs
@@ -2,6 +2,7 @@
 using FOV.Domain.Entities.UserAggregator;
 using FOV.Infrastructure;
 using FOV.Presentation.Infrastructure;
+using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
 var conn = builder.Configuration.GetConnectionString("PostgresConnection");
@@ -11,6 +12,18 @@ builder.Services.AddControllers(options => options.Filters.Add<ValidationExcepti
 builder.Services.AddApplicationDI();
 builder.Services.AddPresentationDI(conn ?? throw new ArgumentNullException(nameof(conn), "Connection string cannot be null."), builder);
 builder.Services.AddInfrastructureDI();
+
+builder.Services.AddOpenTelemetry().WithMetrics(x =>
+{
+    x.AddPrometheusExporter();
+    x.AddMeter("Microsoft.AspNetCore.Hosting",
+        "Microsoft.AspNetCore.Server.Kestrel");
+    x.AddView("request-duration",
+        new ExplicitBucketHistogramConfiguration
+        {
+            Boundaries = [0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.7]
+        });
+});
 
 var app = builder.Build();
 
@@ -27,6 +40,7 @@ app.UseHttpsRedirection();
 app.MapIdentityApi<User>();
 
 
+app.MapPrometheusScrapingEndpoint();
 app.UseCookiePolicy();
 app.UseAuthorization();
 app.UseCors("AllowAllOrigins");
