15b57348dc9b14470719fce05c6b33e17ec7b912
Man Vo <99975060+Nang13@users.noreply.github.com>
2024-09-15 20:20:56 +0700

    deploy: fix

--- src/FOV.Presentation/Program.cs
+++ src/FOV.Presentation/Program.cs
@@ -28,15 +28,12 @@ builder.Services.AddOpenTelemetry().WithMetrics(x =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.ApplyMigrations();
 
 await app.InitializeDatabaseAsync();
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 // Map the default Identity API endpoints except for registration
 app.MapIdentityApi<User>();
 
