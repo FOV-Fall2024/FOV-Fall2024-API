8357d965ef852731d161509fd4fb4e0938483c7b
ducanhnt22 <ducanhbon4@gmail.com>
2024-09-16 15:33:08 +0700

    feat: fix restaurant get response

--- src/FOV.Application/Features/Restaurants/Queries/Get/GetRestaurantQuery.cs
+++ src/FOV.Application/Features/Restaurants/Queries/Get/GetRestaurantQuery.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FOV.Domain.Entities.RestaurantAggregator;
+using FOV.Domain.Entities.RestaurantAggregator.Enums;
 using FOV.Infrastructure.Helpers.GetHelper;
 using FOV.Infrastructure.UnitOfWork.IUnitOfWorkSetup;
 using MediatR;
 
 namespace FOV.Application.Features.Restaurants.Queries.Get;
-public record GetRestaurantCommand(Guid? RestaurantId, string? RestaurantName, string? Address, string? RestaurantPhone, string? RestataurantCode) : IRequest<List<GetRestaurantResponse>>;
-public record GetRestaurantResponse(Guid RestaurantId, string RestaurantName, string Address, string RestaurantPhone, string RestataurantCode);
+public record GetRestaurantCommand(Guid? RestaurantId, string? RestaurantName, string? Address, string? RestaurantPhone, string? RestaurantCode) : IRequest<List<GetRestaurantResponse>>;
+public record GetRestaurantResponse(Guid RestaurantId, string RestaurantName, string Address, string RestaurantPhone, string RestaurantCode, Status RestaurantStatus);
 public class GetRestaurantQuery(IUnitOfWorks unitOfWorks) : IRequestHandler<GetRestaurantCommand, List<GetRestaurantResponse>>
 {
     private readonly IUnitOfWorks _unitOfWorks = unitOfWorks;
@@ -23,9 +24,10 @@ public class GetRestaurantQuery(IUnitOfWorks unitOfWorks) : IRequestHandler<GetR
             RestaurantName = string.IsNullOrEmpty(request.RestaurantName) ? string.Empty : request.RestaurantName,
             Address = string.IsNullOrEmpty(request.Address) ? string.Empty : request.Address,
             RestaurantPhone = string.IsNullOrEmpty(request.RestaurantPhone) ? string.Empty : request.RestaurantPhone,
-            RestataurantCode = string.IsNullOrEmpty(request.RestataurantCode) ? string.Empty : request.RestataurantCode
+            RestataurantCode = string.IsNullOrEmpty(request.RestaurantCode) ? string.Empty : request.RestaurantCode,
+            Status = request.RestaurantId.HasValue ? Status.Active : Status.Inactive
         };
         var filterRestaurant = restaurants.AsQueryable().CustomFilterV1(filterEntity);
-        return filterRestaurant.Select(restaurant => new GetRestaurantResponse(restaurant.Id, restaurant.RestaurantName ?? string.Empty, restaurant.Address ?? string.Empty, restaurant.RestaurantPhone ?? string.Empty, restaurant.RestataurantCode ?? string.Empty)).ToList();
+        return filterRestaurant.Select(restaurant => new GetRestaurantResponse(restaurant.Id, restaurant.RestaurantName ?? string.Empty, restaurant.Address ?? string.Empty, restaurant.RestaurantPhone ?? string.Empty, restaurant.RestataurantCode ?? string.Empty, restaurant.Status)).ToList();
     }
 }
