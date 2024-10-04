3d124f6815b39e5d7437aa3cbbebc4df75da7e60
Man Vo <99975060+Nang13@users.noreply.github.com>
2024-10-04 14:48:44 +0700

    fix

--- src/FOV.Application/Features/Employees/Commands/CreateEmployee/CreateEmployeeValidator.cs
+++ src/FOV.Application/Features/Employees/Commands/CreateEmployee/CreateEmployeeValidator.cs
@@ -6,20 +6,16 @@ using FOV.Infrastructure.UnitOfWork.IUnitOfWorkSetup;
 namespace FOV.Application.Features.Employees.Commands.CreateEmployee;
 public class CreateEmployeeValidator : AbstractValidator<CreateEmployeeCommand>
 {
-    //public CreateEmployeeValidator(RestaurantValidator validations)
-    //{
+    public CreateEmployeeValidator(RestaurantValidator validations)
+    {
 
-    //    RuleFor(x => x.RoleId)
-    //    .NotEmpty()
-    //    .When(x => x.RoleId >= 1 && x.RoleId <= 3);
-    //    RuleFor(x => x.Email)
-    //         .NotNull()
-    //         .Matches(@"^[^@\s]+@[^@\s]+\.[^@\s]+$")
-    //         .WithMessage("Email không hợp lệ.");
-    //    RuleFor(x => x.RestaurantId).SetValidator(validations);
+        RuleFor(x => x.RoleId)
+        .NotEmpty()
+        .When(x => x.RoleId >= 1 && x.RoleId <= 3);
+        RuleFor(x => x.RestaurantId).SetValidator(validations);
 
 
-    //}
+    }
 }
 
 
--- src/FOV.Presentation/Infrastructure/Middleware/ExceptionMiddleware.cs
+++ src/FOV.Presentation/Infrastructure/Middleware/ExceptionMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using FOV.Application.Common.Exceptions;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System.Net;
 
 namespace FOV.Presentation.Infrastructure.Middleware;
@@ -30,14 +31,29 @@ public class ExceptionMiddleware
         context.Response.ContentType = "application/json";
         context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
 
+        var settings = new JsonSerializerSettings
+        {
+            ContractResolver = new LowercaseFirstLetterContractResolver()
+        };
         var result = JsonConvert.SerializeObject(new
         {
             message = exception.Message,
-            statusCode =exception.StatusCode,
+            statusCode =400,
             errors = exception.FieldErrors // Include the detailed errors here
-        });
+        },settings);
 
         return context.Response.WriteAsync(result);
+    }
+
+    public class LowercaseFirstLetterContractResolver : DefaultContractResolver
+    {
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName) || char.IsLower(propertyName[0]))
+                return propertyName;
+
+            return char.ToLower(propertyName[0]) + propertyName.Substring(1);
+        }
     }
 }
 
