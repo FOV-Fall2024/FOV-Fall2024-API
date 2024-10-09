1ff62b9fa7652c77b559f96bad576b9b902c218b
ducanhnt22 <ducanhbon4@gmail.com>
2024-10-09 13:51:13 +0700

    feat: IngredientGeneralDetail fast fix

--- src/FOV.Application/Features/IngredientGenerals/Queries/GetDetailIngredientGeneral/GetDetailIngredientGeneralQuery.cs
+++ src/FOV.Application/Features/IngredientGenerals/Queries/GetDetailIngredientGeneral/GetDetailIngredientGeneralQuery.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FOV.Application.Features.IngredientGenerals.Responses;
+using FOV.Domain.Entities.IngredientGeneralAggregator;
 using FOV.Domain.Entities.IngredientGeneralAggregator.Enums;
 using FOV.Infrastructure.UnitOfWork.IUnitOfWorkSetup;
 using MediatR;
@@ -15,7 +16,8 @@ public class GetDetailIngredientGeneralQuery(IUnitOfWorks unitOfWorks) : IReques
     private readonly IUnitOfWorks _unitOfWorks = unitOfWorks;
     public async Task<GetDetailIngredientGeneralResponse> Handle(GetDetailIngredientGeneralCommand request, CancellationToken cancellationToken)
     {
-        var ingredientGeneral = await _unitOfWorks.IngredientGeneralRepository.GetByIdAsync(request.Id);
-        return new GetDetailIngredientGeneralResponse(ingredientGeneral.Id, ingredientGeneral.IngredientName, ingredientGeneral.IngredientTypeId, ingredientGeneral.IngredientMeasure + 1, ingredientGeneral.IngredientDescription, ingredientGeneral.Status, ingredientGeneral.Created);
+        var allIngredients = await _unitOfWorks.IngredientGeneralRepository.GetAllAsync(x => x.IngredientType);
+        var ingredientGeneral = allIngredients.FirstOrDefault(x => x.Id == request.Id);
+        return new GetDetailIngredientGeneralResponse(ingredientGeneral.Id, ingredientGeneral.IngredientName, ingredientGeneral.IngredientTypeId, ingredientGeneral.IngredientType.IngredientName, ingredientGeneral.IngredientMeasure + 1, ingredientGeneral.IngredientDescription, ingredientGeneral.Status, ingredientGeneral.Created);
     }
 }
--- src/FOV.Application/Features/IngredientGenerals/Responses/Response.cs
+++ src/FOV.Application/Features/IngredientGenerals/Responses/Response.cs
@@ -4,5 +4,5 @@ using FOV.Domain.Entities.TableAggregator.Enums;
 namespace FOV.Application.Features.IngredientGenerals.Responses;
 
 public record GetAllIngredientResponse(Guid Id, string IngredientGeneralName, string IngredientGeneralType, string ingredientMeasureType, string IngredientGeneralDescription, Status Status,DateTime CreatedDate);
-public record GetDetailIngredientGeneralResponse(Guid Id, string IngredientGeneralName, Guid IngredientGeneralType, IngredientMeasure IngredientMeasureType, string IngredientGeneralDescription, Status Status, DateTime CreatedDate);
+public record GetDetailIngredientGeneralResponse(Guid Id, string IngredientGeneralName, Guid IngredientGeneralType, string IngredientGeneralTypeName, IngredientMeasure IngredientMeasureType, string IngredientGeneralDescription, Status Status, DateTime CreatedDate);
 
