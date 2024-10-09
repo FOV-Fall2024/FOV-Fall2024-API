8940b9ed5c6d737924f0538e9e0004ce4af633ab
ducanhnt22 <ducanhbon4@gmail.com>
2024-10-09 14:38:20 +0700

    feat: IngredientGeneralDetail fast fix

--- src/FOV.Application/Features/IngredientGenerals/Commands/Update/UpdateIngredientGeneralValidator.cs
+++ src/FOV.Application/Features/IngredientGenerals/Commands/Update/UpdateIngredientGeneralValidator.cs
@@ -1,32 +1,31 @@
 ﻿using FluentValidation;
 using FOV.Application.Features.IngredientGenerals.Commands.Active;
+using FOV.Domain.Entities.IngredientAggregator;
 using FOV.Domain.Entities.IngredientGeneralAggregator;
 using FOV.Infrastructure.UnitOfWork.IUnitOfWorkSetup;
 
 namespace FOV.Application.Features.IngredientGenerals.Commands.Update;
 
-public class UpdateIngredientGeneralValidator : AbstractValidator<UpdateIngredientGeneralCommand>
-{
-    public UpdateIngredientGeneralValidator(IngredientGeneralIdValidator validator, IngredientNameValidator name)
-    {
-        RuleFor(x => x.Id).NotEmpty().SetValidator(validator);
-        RuleFor(x => x.IngredientGeneralName).NotEmpty().SetValidator(name);
-    }
-}
-
-public sealed class IngredientNameValidator : AbstractValidator<string>
+public sealed class IngredientNameValidator : AbstractValidator<UpdateIngredientGeneralCommand>
 {
     private readonly IUnitOfWorks _unitOfWorks;
     public IngredientNameValidator(IUnitOfWorks unitOfWorks)
     {
         _unitOfWorks = unitOfWorks;
-        RuleFor(name => name).MustAsync(CheckDuplicateName).WithMessage("Tên này đã có trong hệ thống");
-
+        RuleFor(x => x).MustAsync(CheckDuplicateName).WithMessage("Tên này đã có trong hệ thống");
     }
-
-    private async Task<bool> CheckDuplicateName(string name, CancellationToken token)
+    private async Task<bool> CheckDuplicateName(UpdateIngredientGeneralCommand command, CancellationToken token)
     {
-        IngredientGeneral? ingredientGeneral = await _unitOfWorks.IngredientGeneralRepository.FirstOrDefaultAsync(x => x.IngredientName == name);
+        IngredientGeneral? ingredientGeneral = await _unitOfWorks.IngredientGeneralRepository.FirstOrDefaultAsync(x => x.IngredientName == command.IngredientGeneralName && x.Id != command.Id);
         return ingredientGeneral == null;
+    }
+}
+
+public class UpdateIngredientGeneralValidator : AbstractValidator<UpdateIngredientGeneralCommand>
+{
+    public UpdateIngredientGeneralValidator(IngredientGeneralIdValidator validator, IngredientNameValidator nameValidator)
+    {
+        RuleFor(x => x.Id).NotEmpty().SetValidator(validator);
+        RuleFor(x => x).SetValidator(nameValidator);
     }
 }
