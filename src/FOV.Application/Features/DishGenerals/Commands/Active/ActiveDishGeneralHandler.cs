9db8e609cc5eccf408a4c33525a714db953ce847
ducanhnt22 <ducanhbon4@gmail.com>
2024-10-10 11:20:57 +0700

    feat: activeDishGeneral;

--- src/FOV.Application/Features/DishGenerals/Commands/Active/ActiveDishGeneralHandler.cs
+++ src/FOV.Application/Features/DishGenerals/Commands/Active/ActiveDishGeneralHandler.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentResults;
+using FOV.Domain.Entities.DishGeneralAggregator;
+using FOV.Infrastructure.UnitOfWork.IUnitOfWorkSetup;
 using MediatR;
 
 namespace FOV.Application.Features.DishGenerals.Commands.Active;
 
 public sealed record ActiveProductGeneralCommand(Guid id) : IRequest<Result>;
-//public class ActiveProductGeneralHandler : IRequestHandler<Result>
-//{
-
-//}
+public class ActiveProductGeneralHandler(IUnitOfWorks unitOfWorks) : IRequestHandler<ActiveProductGeneralCommand, Result>
+{
+    private readonly IUnitOfWorks _unitOfWorks = unitOfWorks;
+    public async Task<Result> Handle(ActiveProductGeneralCommand request, CancellationToken cancellationToken)
+    {
+        DishGeneral productGeneral = await _unitOfWorks.DishGeneralRepository.GetByIdAsync(request.id) ?? throw new Exception();
+        productGeneral.SetState(false);
+        _unitOfWorks.DishGeneralRepository.Update(productGeneral);
+        await _unitOfWorks.SaveChangeAsync();
+        return Result.Ok();
+    }
+}
