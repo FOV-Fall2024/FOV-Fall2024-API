e4158be1a170b54f66d16dfd601b6c40e9907f8d
ducanhnt22 <ducanhbon4@gmail.com>
2024-10-12 15:26:59 +0700

    feat: DishGeneral Detail

--- src/FOV.Application/Features/DishGenerals/Responses/Response.cs
+++ src/FOV.Application/Features/DishGenerals/Responses/Response.cs
@@ -1,9 +1,10 @@
-﻿using FOV.Domain.Entities.TableAggregator.Enums;
+﻿using FOV.Domain.Entities.IngredientGeneralAggregator.Enums;
+using FOV.Domain.Entities.TableAggregator.Enums;
 
 namespace FOV.Application.Features.DishGenerals.Responses;
 
 public record GetProductGeneralResponse(Guid Id, string DishGeneralName, decimal DishGeneralPrice, string DishGeneralDescription, Status Status, string CategoryName, DateTime CreatedDate, DateTime UpdateTime, decimal PercentagePriceDifference, List<GetAdditionalImage> Images);
 
-public record GetProductGeneralDetailResponse(Guid Id, string DishGeneralName, string DishGeneralDescription, DateTime CreatedDate, DateTime UpdateDated, List<GetIngredientResponse> GetIngredients,IEnumerable<GetAdditionalImage> Images);
-public record GetIngredientResponse(Guid IngredientGeneralId, string IngredientGeneralName, decimal IngredientGeneralQuantity, DateTime CreatedDate);
+public record GetProductGeneralDetailResponse(Guid Id, string DishGeneralName, string DishGeneralDescription, DateTime CreatedDate, DateTime UpdateDated, List<GetIngredientResponse> GetIngredients,IEnumerable<GetAdditionalImage> Images, decimal DishGeneralPrice, decimal PercentagePriceDifference);
+public record GetIngredientResponse(Guid IngredientGeneralId, string IngredientGeneralName, decimal IngredientGeneralQuantity, string IngredientMeasureName, DateTime CreatedDate);
 public sealed record GetAdditionalImage(Guid DishGeneralImageId,string Url);
