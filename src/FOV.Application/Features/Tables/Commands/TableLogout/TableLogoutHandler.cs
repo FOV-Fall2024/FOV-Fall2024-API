5d4cc49c1885b44ecb1a0d3a13dc895b6ac6a25f
ducanhnt22 <ducanhbon4@gmail.com>
2024-10-24 18:03:24 +0700

    feat: table logout, add authorize

---
+++ src/FOV.Application/Features/Tables/Commands/TableLogout/TableLogoutHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using FOV.Infrastructure.UnitOfWork.IUnitOfWorkSetup;
+using MediatR;
+
+namespace FOV.Application.Features.Tables.Commands.TableLogout;
+public record TableLogoutCommand(Guid TableId, string EmployeeCode) : IRequest<Guid>;
+public partial class TableLogoutHandler(IUnitOfWorks unitOfWorks) : IRequestHandler<TableLogoutCommand, Guid>
+{
+    private readonly IUnitOfWorks _unitOfWorks = unitOfWorks;
+    public async Task<Guid> Handle(TableLogoutCommand request, CancellationToken cancellationToken)
+    {
+        if (!MyRegex().IsMatch(request.EmployeeCode))
+        {
+            throw new Exception("Invalid Manager Code format.");
+        }
+
+        var table = await _unitOfWorks.TableRepository.GetByIdAsync(request.TableId) ?? throw new Exception();
+        var restaurantId = table.RestaurantId;
+        var manager = await _unitOfWorks.EmployeeRepository.WhereAsync(x => x.RestaurantId == restaurantId && x.EmployeeCode == request.EmployeeCode);
+
+        if (!manager.Any())
+        {
+            throw new Exception("Manager not found or does not belong to this restaurant.");
+        }
+
+        table.UpdateIsLogin(false);
+        _unitOfWorks.TableRepository.Update(table);
+        await _unitOfWorks.SaveChangeAsync();
+        return table.Id;
+    }
+
+    [GeneratedRegex(@"^MNG_\d+$")]
+    private static partial Regex MyRegex();
+}
--- src/FOV.Presentation/Controllers/V1/TableController.cs
+++ src/FOV.Presentation/Controllers/V1/TableController.cs
@@ -3,11 +3,14 @@ using FOV.Application.Features.Tables.Commands.Active;
 using FOV.Application.Features.Tables.Commands.Create;
 using FOV.Application.Features.Tables.Commands.Inactive;
 using FOV.Application.Features.Tables.Commands.TableLogin;
+using FOV.Application.Features.Tables.Commands.TableLogout;
 using FOV.Application.Features.Tables.Queries;
+using FOV.Domain.Entities.UserAggregator.Enums;
 using FOV.Infrastructure.Helpers.GetHelper;
 using FOV.Presentation.Controllers.V1;
 using FOV.Presentation.Infrastructure.Core;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FOV.Presentation.Controllers.v1;
@@ -15,6 +18,7 @@ public class TableController(ISender sender) : DefaultController
 {
     private readonly ISender _sender = sender;
 
+    [Authorize(Roles = Role.Manager)]
     [HttpPost("{restaurantId:guid}")]
     public async Task<IActionResult> Add(Guid restaurantId, [FromQuery] int Amount)
     {
@@ -29,12 +33,14 @@ public class TableController(ISender sender) : DefaultController
     //    var response = await _sender.Send(command);
     //    return Ok(response);
     //}
+    [Authorize(Roles = Role.Manager)]
     [HttpPost("{id:guid}/active")]
     public async Task<IActionResult> Active(Guid id)
     {
         var response = await _sender.Send(new ActiveTableCommand(id));
         return Ok(new OK_Result<Guid>("Đổi trạng thái của bàn thành công", response));
     }
+    [Authorize(Roles = Role.Manager)]
     [HttpPost("{id:guid}/inactive")]
     public async Task<IActionResult> Inactive(Guid id)
     {
@@ -46,6 +52,13 @@ public class TableController(ISender sender) : DefaultController
     {
         var response = await _sender.Send(new TableLoginCommand(id));
         return Ok(new OK_Result<Guid>("Đăng nhập bàn thành công", response));
+    }
+    [Authorize(Roles = Role.Manager)]
+    [HttpPatch("{id:guid}/TableLogout")]
+    public async Task<IActionResult> TableLogout(Guid id, [FromQuery] string employeeCode)
+    {
+        var response = await _sender.Send(new TableLogoutCommand(id, employeeCode));
+        return Ok(new OK_Result<Guid>("Đăng xuất bàn thành công", response));
     }
     [HttpGet]
     public async Task<IActionResult> Get([FromQuery] GetTableCommand command)
