b4695af1ad0a18131ae4c49970776bd8d20ff317
Man Vo <99975060+Nang13@users.noreply.github.com>
2024-11-16 13:12:01 +0700

    feat

--- src/FOV.Application/Features/IngredientTypes/Commands/Create/CreateIngredientTypeValidator.cs
+++ src/FOV.Application/Features/IngredientTypes/Commands/Create/CreateIngredientTypeValidator.cs
@@ -7,14 +7,33 @@ using FOV.Infrastructure.UnitOfWork.IUnitOfWorkSetup;
 namespace FOV.Application.Features.IngredientTypes.Commands.Create;
 public class CreateIngredientTypeValidator : AbstractValidator<CreateChildIngredientTypeCommand>
 {
-    public CreateIngredientTypeValidator(IngredientTypeValidator validator, CheckIngredientParentIdValidator validationRules)
+    public CreateIngredientTypeValidator(IngredientTypeCheckNameValidator validator, CheckIngredientParentIdValidator validationRules)
     {
         RuleFor(x => x.IngredientTypeName).NotEmpty().SetValidator(validator);
 
     }
 }
 
+public sealed class IngredientTypeCheckNameValidator : AbstractValidator<string>
+{
+    private readonly IUnitOfWorks _unitOfWorks;
+    public IngredientTypeCheckNameValidator(IUnitOfWorks unitOfWorks)
+    {
+        _unitOfWorks = unitOfWorks;
 
+        RuleFor(name => name)
+            .MustAsync(CheckDuplicateName)
+            .WithMessage("Tên đã tồn tại trong hệ thống");
+
+    }
+
+    private async Task<bool> CheckDuplicateName(string name, CancellationToken token)
+    {
+        var existingIngredient = await _unitOfWorks.IngredientTypeRepository
+           .FirstOrDefaultAsync(x => x.IngredientName == name);
+        return existingIngredient == null;
+    }
+}
 public sealed class CheckIngredientParentIdValidator : AbstractValidator<Guid>
 {
     private readonly IUnitOfWorks _unitOfWorks;
