d8f41da697ffc267b28fcc6ea5c98f3a3098b04b
Man Vo <99975060+Nang13@users.noreply.github.com>
2024-11-16 16:20:51 +0700

    fix

--- src/FOV.Application/Features/DishGenerals/Mapper/DishGeneralMapper.cs
+++ src/FOV.Application/Features/DishGenerals/Mapper/DishGeneralMapper.cs
@@ -9,11 +9,11 @@ public static class DishGeneralMapper
 
     public static GetIngredientResponse MapperIngredientDTO(this IngredientGeneral ingredient, Guid productId)
     {
-        return new GetIngredientResponse(ingredient.Id, ingredient.IngredientName, ingredient.DishIngredientGenerals.Where(x => x.DishGeneralId == productId).FirstOrDefault().Quantity, ingredient.IngredientMeasure.ToString() ,ingredient.Created);
+        return new GetIngredientResponse(ingredient.Id, ingredient.IngredientName, ingredient.DishIngredientGenerals.Where(x => x.DishGeneralId == productId).FirstOrDefault().Quantity, ingredient.IngredientMeasure.ToString(), ingredient.Created);
     }
 
     public static GetProductGeneralDetailResponse MapperDetailDTO(this DishGeneral dishGeneral, List<GetIngredientResponse>? getIngredient)
     {
-        return new GetProductGeneralDetailResponse(dishGeneral.Id, dishGeneral.DishName, dishGeneral.DishDescription, dishGeneral.Created, dishGeneral.LastModified ?? DateTime.Now, getIngredient ?? [], dishGeneral.DishGeneralImages.Select(x =>new GetAdditionalImage(x.Id,x.Url)), dishGeneral.Price, dishGeneral.PercentagePriceDifference);
+        return new GetProductGeneralDetailResponse(dishGeneral.Id, dishGeneral.DishName, dishGeneral.DishDescription, dishGeneral.Created, dishGeneral.LastModified ?? DateTime.Now, getIngredient ?? [], dishGeneral.DishGeneralImages.Select(x => new GetAdditionalImage(x.Id, x.Url)), dishGeneral.Price, dishGeneral.PercentagePriceDifference);
     }
 }
