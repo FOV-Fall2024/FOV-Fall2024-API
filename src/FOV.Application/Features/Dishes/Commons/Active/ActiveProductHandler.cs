ccd91853ae3c44995891636128fd6062bdf6ba74
ducanhnt22 <ducanhbon4@gmail.com>
2024-11-17 19:08:06 +0700

    feat: check active dish

--- src/FOV.Application/Features/Dishes/Commons/Active/ActiveProductHandler.cs
+++ src/FOV.Application/Features/Dishes/Commons/Active/ActiveProductHandler.cs
@@ -1,5 +1,6 @@
 ﻿using FluentResults;
 using FOV.Domain.Entities.DishAggregator;
+using FOV.Domain.Entities.TableAggregator.Enums;
 using FOV.Infrastructure.UnitOfWork.IUnitOfWorkSetup;
 using MediatR;
 
@@ -11,6 +12,16 @@ public class ActiveProductHandler(IUnitOfWorks unitOfWorks) : IRequestHandler<Ac
     public async Task<Result> Handle(ActiveProductCommand request, CancellationToken cancellationToken)
     {
         Dish product = await _unitOfWorks.DishRepository.GetByIdAsync(request.Id) ?? throw new Exception();
+
+        var dishGeneral = await _unitOfWorks.DishGeneralRepository.GetByIdAsync(product.DishGeneralId.Value) ?? throw new Exception();
+        if (dishGeneral.Status == Status.Inactive)
+        {
+            return Result.Fail("Không thể kích hoạt món ăn này vì món này đã bị admin vô hiệu hóa");
+        }
+        if (product.Status == Status.Active)
+        {
+            return Result.Fail("Món ăn đã được kích hoạt");
+        }
         product.UpdateState(true);
         _unitOfWorks.DishRepository.Update(product);
         await _unitOfWorks.SaveChangeAsync();
