710021f68adbd27a5d4074fc61c421522d7a66dc
ducanhnt22 <ducanhbon4@gmail.com>
2024-11-17 19:15:43 +0700

    feat: check active dish

--- src/FOV.Application/Features/Dishes/Commons/Active/ActiveProductHandler.cs
+++ src/FOV.Application/Features/Dishes/Commons/Active/ActiveProductHandler.cs
@@ -1,31 +1,32 @@
 ﻿using FluentResults;
+using FOV.Application.Common.Exceptions;
 using FOV.Domain.Entities.DishAggregator;
 using FOV.Domain.Entities.TableAggregator.Enums;
 using FOV.Infrastructure.UnitOfWork.IUnitOfWorkSetup;
 using MediatR;
 
 namespace FOV.Application.Features.Dishes.Commons.Active;
-public sealed record ActiveProductCommand(Guid Id) : IRequest<Result>;
-public class ActiveProductHandler(IUnitOfWorks unitOfWorks) : IRequestHandler<ActiveProductCommand, Result>
+public sealed record ActiveProductCommand(Guid Id) : IRequest<Guid>;
+public class ActiveProductHandler(IUnitOfWorks unitOfWorks) : IRequestHandler<ActiveProductCommand, Guid>
 {
     private readonly IUnitOfWorks _unitOfWorks = unitOfWorks;
-    public async Task<Result> Handle(ActiveProductCommand request, CancellationToken cancellationToken)
+    public async Task<Guid> Handle(ActiveProductCommand request, CancellationToken cancellationToken)
     {
-        Dish product = await _unitOfWorks.DishRepository.GetByIdAsync(request.Id) ?? throw new Exception();
+        Dish product = await _unitOfWorks.DishRepository.GetByIdAsync(request.Id) ?? throw new AppException("Không tìm thấy món ăn");
 
-        var dishGeneral = await _unitOfWorks.DishGeneralRepository.GetByIdAsync(product.DishGeneralId.Value) ?? throw new Exception();
+        var dishGeneral = await _unitOfWorks.DishGeneralRepository.GetByIdAsync(product.DishGeneralId.Value) ?? throw new AppException("Không tìm thấy món ăn gốc");
         if (dishGeneral.Status == Status.Inactive)
         {
-            return Result.Fail("Không thể kích hoạt món ăn này vì món này đã bị admin vô hiệu hóa");
+            throw new AppException("Không thể kích hoạt món ăn này vì món này đã bị admin vô hiệu hóa");
         }
         if (product.Status == Status.Active)
         {
-            return Result.Fail("Món ăn đã được kích hoạt");
+            throw new AppException("Món ăn đã được kích hoạt");
         }
         product.UpdateState(true);
         _unitOfWorks.DishRepository.Update(product);
         await _unitOfWorks.SaveChangeAsync();
-        return Result.Ok();
+        return product.Id;
 
     }
 }
