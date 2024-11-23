11849877760603960fc91ac7f178a4ede70490f4
Man Vo <99975060+Nang13@users.noreply.github.com>
2024-11-23 11:47:29 +0700

    feat:

--- src/FOV.Application/Features/Dishes/Queries/GetDetail/GetDetailQuery.cs
+++ src/FOV.Application/Features/Dishes/Queries/GetDetail/GetDetailQuery.cs
@@ -13,7 +13,7 @@ public class GetDetailQuery(IUnitOfWorks unitOfWorks) : IRequestHandler<GetDetai
     public async Task<GetDetailResponse> Handle(GetDetailCommand request, CancellationToken cancellationToken)
     {
         Dish? dish = await _unitOfWorks.DishRepository.GetByIdAsync(request.Id, x => x.DishGeneral.DishGeneralImages, x => x.DishIngredients, x => x.Category);
-        var ingredient = await _unitOfWorks.DishIngredientRepository.WhereAsync(x => x.DishId == request.Id, x => x.Ingredient, x => x.Ingredient.IngredientType,x => x.Ingredient.IngredientGeneral);
+        var ingredient = await _unitOfWorks.DishIngredientRepository.WhereAsync(x => x.DishId == request.Id, x => x.Ingredient, x => x.Ingredient.IngredientType, x => x.Ingredient.IngredientGeneral, x => x.Ingredient.IngredientGeneral.IngredientMeasure);
 
         return dish.MapperDetailDTO(dish.DishGeneral.DishGeneralImages.Select(x => x.Url).ToList(), ingredient);
 
