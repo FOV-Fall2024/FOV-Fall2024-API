5acf31c6d064e2af6ba3c36494d0f2c9679fa0d0
ducanhnt22 <ducanhbon4@gmail.com>
2024-11-27 15:41:35 +0700

    feat: statistic

--- src/FOV.Application/Features/Statistics/Queries/GetCurrentStatistic/GetCurrentStatisticQuery.cs
+++ src/FOV.Application/Features/Statistics/Queries/GetCurrentStatistic/GetCurrentStatisticQuery.cs
@@ -4,20 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FOV.Application.Common.Behaviours.Claim;
+using FOV.Domain.Entities.TableAggregator.Enums;
+using FOV.Domain.Entities.UserAggregator;
 using FOV.Domain.Entities.UserAggregator.Enums;
 using FOV.Infrastructure.UnitOfWork.IUnitOfWorkSetup;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace FOV.Application.Features.Statistics.Queries.GetCurrentStatistic;
 public record GetCurrentStatisticCommand : IRequest<CurrentStatisticDto>;
-public record CurrentStatisticDto(RevenueStatistic RevenueStatistic, OrderStatistic OrderStatistic, CustomerStatistic CustomerStatistic, int? TotalBranches);
+public record CurrentStatisticDto(RevenueStatistic RevenueStatistic, OrderStatistic OrderStatistic, CustomerStatistic CustomerStatistic, int? TotalBranches, int? TotalEmployees);
 public record RevenueStatistic(decimal TotalRevenue, decimal PercentageAfterLastMonth);
 public record OrderStatistic(int TotalOrder, decimal PercentageAfterLastMonth);
 public record CustomerStatistic(int TotalCustomer, decimal PercentageAfterLastMonth);
-public class GetCurrentStatisticQuery(IUnitOfWorks unitOfWorks, IClaimService claimService) : IRequestHandler<GetCurrentStatisticCommand, CurrentStatisticDto>
+public class GetCurrentStatisticQuery(IUnitOfWorks unitOfWorks, IClaimService claimService, UserManager<User> userManager) : IRequestHandler<GetCurrentStatisticCommand, CurrentStatisticDto>
 {
     private readonly IUnitOfWorks _unitOfWorks = unitOfWorks;
     private readonly IClaimService _claimService = claimService;
+    private readonly UserManager<User> _userManager = userManager;
     public async Task<CurrentStatisticDto> Handle(GetCurrentStatisticCommand request, CancellationToken cancellationToken)
     {
         var userRole = _claimService.UserRole;
@@ -50,16 +55,22 @@ public class GetCurrentStatisticQuery(IUnitOfWorks unitOfWorks, IClaimService cl
         var previousCustomers = customers.Count(c => c.Created >= previousMonthStart && c.Created <= previousMonthEnd);
         var customersPercentage = CalculatePercentageChange(currentCustomers, previousCustomers);
 
-        int? totalBranch = null;
+        int? totalBranch = 0;
         if (userRole == Role.Administrator)
         {
             totalBranch = await _unitOfWorks.RestaurantRepository.CountAsync();
+        }
+        int? totalEmployee = 0;
+        if (userRole == Role.Manager)
+        {
+            totalEmployee = await _userManager.Users.Where(x => x.RestaurantId == restaurantId && x.Status == Status.Active && (x.EmployeeCode.StartsWith("WTR_") || x.EmployeeCode.StartsWith("CKR_"))).CountAsync();
         }
 
         return new CurrentStatisticDto(new RevenueStatistic(currentRevenue, revenuePercentage),
                                        new OrderStatistic(currentOrders, ordersPercentage),
                                        new CustomerStatistic(currentCustomers, customersPercentage),
-                                       totalBranch);
+                                       totalBranch,
+                                       totalEmployee);
     }
     private static decimal CalculatePercentageChange(decimal current, decimal previous)
     {
