a481f1e163eef4c3f062c3c368b0e06846d41fc9
Man Vo <99975060+Nang13@users.noreply.github.com>
2024-11-27 21:34:23 +0700

    build: testNotify

--- src/FOV.Presentation/Controllers/V1/TokenController.cs
+++ src/FOV.Presentation/Controllers/V1/TokenController.cs
@@ -1,3 +1,4 @@
+using FOV.Infrastructure.FCM;
 using FOV.Infrastructure.FirebaseDB;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +11,13 @@ public class FCMTokenController : DefaultController
     public async Task<IActionResult> AddToken(Guid userId,string tokenId) 
     {
         await FCMTokenHandler.AddFCMToken(userId,tokenId);
+        return Ok();
+    }
+
+    [HttpPost("{token}/test-notify")]
+    public async Task<IActionResult> TestNotify(string token)
+    {
+        await CloudMessagingHandlers.SendPushNotification(token, "hih", "hiih");
         return Ok();
     }
 }
