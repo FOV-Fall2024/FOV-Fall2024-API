c4ebb173961bbbc4d3f2b92fd8aedeb324eb9ffc
ducanhnt22 <ducanhbon4@gmail.com>
2024-12-09 02:36:56 +0700

    feat: comment local test code

--- src/FOV.Application/Features/Payments/Commands/CreateVNPayPayment/VNPayReturnHandler.cs
+++ src/FOV.Application/Features/Payments/Commands/CreateVNPayPayment/VNPayReturnHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using FOV.Domain.Entities.OrderAggregator.Enums;
 using FOV.Domain.Entities.TableAggregator.Enums;
 using FOV.Infrastructure.Helpers.VNPayHelper;
 using FOV.Infrastructure.UnitOfWork.IUnitOfWorkSetup;
