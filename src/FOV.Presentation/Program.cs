aca864e2e8f981ed1ea5bfb8a60480ba56b01fc1
Man Vo <99975060+Nang13@users.noreply.github.com>
2024-12-16 12:16:57 +0700

    hot-fix

--- src/FOV.Presentation/Program.cs
+++ src/FOV.Presentation/Program.cs
@@ -1,19 +1,16 @@
 ﻿using FOV.Application;
-using FOV.Domain.Entities.UserAggregator;
-using FOV.Infrastructure;
-using FOV.Infrastructure.Data;
 using FOV.Infrastructure.Notifications.Web.SignalR.Notification.Setup;
 using FOV.Infrastructure.Notifications.Web.SignalR.Order.Setup;
-using FOV.Presentation.Infrastructure;
+using FOV.Infrastructure;
 using FOV.Presentation.Infrastructure.Middleware;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using FOV.Presentation.Infrastructure;
 using OpenTelemetry.Metrics;
 using Prometheus;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
 var builder = WebApplication.CreateBuilder(args);
 var conn = builder.Configuration.GetConnectionString("PostgresConnection");
+
 // Add services to the container
 builder.Services.AddInfrastructureDI();
 builder.Services.AddApplicationDI();
@@ -30,19 +27,18 @@ builder.Services.AddCors(options =>
     });
 });
 
-
 builder.Services.AddOpenTelemetry().WithMetrics(x =>
 {
     x.AddPrometheusExporter();
-    x.AddMeter("Microsoft.AspNetCore.Hosting",
-        "Microsoft.AspNetCore.Server.Kestrel");
-    x.AddView("request-duration",
-        new ExplicitBucketHistogramConfiguration
-        {
-            Boundaries = [0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.7]
-        });
+    x.AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel");
+    x.AddView("request-duration", new ExplicitBucketHistogramConfiguration
+    {
+        Boundaries = [0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.7]
+    });
 });
+
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -50,50 +46,48 @@ app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-
-    // Controls how models are expanded in the UI
-    c.DefaultModelExpandDepth(0); // Models won't be expanded by default
-    c.DefaultModelsExpandDepth(-1); // Disable the "Models" section
-    c.DefaultModelRendering(ModelRendering.Example); // Show examples as the default view
-
-    // Displaying additional helpful information
-    c.DisplayOperationId(); // Show operation IDs
-    c.DisplayRequestDuration(); // Show request duration in the UI
-
-    // Control how endpoints are shown
-    c.DocExpansion(DocExpansion.None); // Endpoints are collapsed by default
-
-    // Enable advanced features
-    c.EnableDeepLinking(); // Generate links for each section
-    c.EnableFilter(); // Adds a filter box to search for operations
-    c.ShowExtensions(); // Show vendor extensions in the UI
-    c.EnableValidator(); // Enables swagger schema validation
-
-    
+    c.DefaultModelExpandDepth(0);
+    c.DefaultModelsExpandDepth(-1);
+    c.DefaultModelRendering(ModelRendering.Example);
+    c.DisplayOperationId();
+    c.DisplayRequestDuration();
+    c.DocExpansion(DocExpansion.None);
+    c.EnableDeepLinking();
+    c.EnableFilter();
+    c.ShowExtensions();
+    c.EnableValidator();
 });
 
 app.ApplyMigrations();
 
 await app.InitializeDatabaseAsync();
+
 app.UseHttpsRedirection();
-// Map the default Identity API endpoints except for registration
-app.MapHub<OrderHub>("order-hub").RequireCors("CorsPolicy");
-app.MapHub<NotificationHub>("notification-hub").RequireCors("CorsPolicy");
-//app.MapIdentityApi<User>();
+
+// Add UseRouting here before UseEndpoints
+app.UseRouting();
+
+// Middleware that runs after routing and before endpoints
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.MapPrometheusScrapingEndpoint();
 app.UseCookiePolicy();
-
 app.UseHttpMetrics();
-app.MapMetrics();
 
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseCors("CorsPolicy");
+
+// Map the default Identity API endpoints except for registration
+app.MapHub<OrderHub>("order-hub").RequireCors("CorsPolicy");
+app.MapHub<NotificationHub>("notification-hub").RequireCors("CorsPolicy");
+// app.MapIdentityApi<User>();
+
+// UseEndpoints after UseRouting
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
-    endpoints.MapMetrics(); // Exposes the /metrics endpoint
+    endpoints.MapMetrics(); // This exposes the /metrics endpoint for Prometheus
+    endpoints.MapPrometheusScrapingEndpoint();
 });
+
 app.Run();
